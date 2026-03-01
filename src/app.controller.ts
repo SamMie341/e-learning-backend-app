4af621c5af63a3d985c74ce72b9d696a01d002df
SamMie341 <soukthavixay2018@gmail.com>
2026-03-01 18:25:57 +0700

    edit show hello

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,14 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      message: 'E-Learning API is running successfully',
+      version: '1.0.0',
+      status: 'Active',
+    };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,11 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello() {
-    return {
-      message: 'E-Learning API is running successfully',
-      version: '1.0.0',
-      status: 'Active',
-    };
+  getHello(): string {
+    return 'Hello World!';
   }
 }
